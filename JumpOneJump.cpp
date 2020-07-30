6238d3e7ae624cc926f5f429d6a759a7cb59dae2
qgjyf2001 <qgjyf2001@163.com>
2020-07-30 14:51:12 +0800

    更新，棋子头部比木块高的bug正在修复中

--- JumpOneJump.cpp
+++ JumpOneJump.cpp
@@ -8,9 +8,11 @@
 using namespace std;
 COLORREF maps[1024][768];
 #define person RGB(56,59,102)
+vector<COLORREF> background = { RGB(171, 167, 119),RGB(133,136,160),RGB(140,142,149),RGB(168,149,148),RGB(142,160,154),RGB(168,147,114) };
 #define sqr(x) ((x)*(x))
-#define eps 5
+#define eps 8
 #define DEBUG_R 3
+#define maxX 440
 struct  pos
 {
     pos(int _x,int _y):x(_x),y(_y)
@@ -51,6 +53,13 @@ void debug(HDC now, int x, int y)
             for (int yy = y - DEBUG_R; yy < y + DEBUG_R; yy++)
                 SetPixel(now, xx, yy, RGB(100, 100, 100));
 }
+void debugLine(HDC now, int y)
+{
+    printf("start debug");
+    for (int i = 1; i <= 40; i++)
+        for (int x = 0; x < maxX; x++)
+            SetPixel(now, x, y, RGB(100, 100, 100));
+}
 #define DEBUG(now) cout<<(int)GetRValue(now)<<" "<<(int)GetGValue(now)<<" "<<(int)GetBValue(now)<<"\n"
 void   GetCaptureBmp(HWND now,LONG width,LONG height)
 {
@@ -80,55 +89,28 @@ void   GetCaptureBmp(HWND now,LONG width,LONG height)
     DeleteDC(MemDC); 
 
 }   
-int getBoxYpos(int width,int y)//扫描分析
-{
-    int check = 0;
-    int maxn = 0;
-    for (int j = 0; j < y; j++)
-    {
-        vector<COLORREF> temp;
-        for (int i = 0; i < width; i++)
-            temp.push_back(maps[i][j]);
-        auto result = count(temp);
-       sort(result.begin(), result.end(), [](auto a, auto b)
-            {
-                return a.second > b.second;
-            });
-       if (result[1].second > 80)
-       {
-           if (!check)
-               check = 1;
-           if (check == 1)
-               if (result[1].second > maxn)
-                   maxn = result[1].second;
-               else
-               {
-                   tmpColor = result[1].first;
-                   return j;
-               }
-       }
-    }
-}
-int getBoxXPos(int width,int height,int y,int maxY)
+int getBoxXPos(int width,int y,int px)
 {        
-    vector<pair<int, int>> total;
-    for (int i = 0; i <= width; i++)
-        if (maps[i][y] == tmpColor)
-        {
+    //98 345
+    //133 345
+#define head RGB(52,52,59)
+    for (int j = 200; j < y; j++)
+        for (int i = 0; i <= width-20; i++)
+            if (maps[i][j] != maps[maxX][j])
+            {
+                if (checkDistance(maps[i][j], head))
+                    return px + (y - j)/5*4;
+                return i;
+                
+            }
 
-            int temp=0;
-            for (int j = 0; j < maxY; j++)
-                if (maps[i][j] == tmpColor)
-                    temp++;
-            total.push_back(move(make_pair(temp, i)));
-    }
-    return max_element(total.begin(), total.end(), [](auto x, auto y)
-        {
-            return x.first < y.first;
-        })->second;
+
 }
 int main()
 {
+    double times = 3.783;
+    printf("进行下一步前需要打开跳一跳点击开始游戏\n请输入倍率，按回车确定(推荐值3.783)");
+   // cin >> times;
     auto window = ::GetDesktopWindow();
     window = ::FindWindowEx(window, 0, NULL, L"跳一跳"); 
     RECT rect;        
@@ -136,11 +118,11 @@ int main()
     GetWindowRect(window, &rect);   
 /*SendMessage(window, WM_LBUTTONDOWN, MK_LBUTTON, MAKELPARAM(100,100));
     Sleep(5000);
-    SendMessage(window, WM_LBUTTONUP, 0, MAKELPARAM(100,100));*/
+    SendMessage(window, WM_LBUTTONUP, 0, MAKELPARAM(100,100));*/     
+    auto dc = GetDC(window);
     while (true)
     {
         GetCaptureBmp(window, rect.right - rect.left, rect.bottom - rect.top);
-        auto dc = GetDC(window);
         // debug
         auto p = pos(0, 0);
         for (int j = 0; j < 768; j++)
@@ -153,14 +135,16 @@ int main()
                 p = pos((i + p.x) / 2, p.y);
                 break;
             }
-        debug(dc, p.x, p.y);
-        auto yPos = getBoxYpos(rect.right - rect.left, p.y);
-        auto xPos = getBoxXPos(rect.right - rect.left, rect.bottom - rect.top, yPos, p.y);
-        auto next = pos(xPos, yPos);
+        
+        //auto yPos = getBoxYpos(rect.right - rect.left, p.y);
+        auto xPos = getBoxXPos(rect.right - rect.left, p.y,p.x);
+       /* auto next = pos(xPos, yPos);
         debug(dc, next.x, next.y);
-        SendMessage(window, WM_LBUTTONDOWN, 0, MAKELPARAM(100, 100));
-        Sleep((float)distance(next, p) * 3.27);     
+        debugLine(dc, next.y);*/
+       SendMessage(window, WM_LBUTTONDOWN, 0, MAKELPARAM(100, 100));
+        Sleep((float)(fabs(xPos-p.x)) * times);     
         SendMessage(window, WM_LBUTTONUP, 0, MAKELPARAM(100, 100));
+       
         Sleep(2000);
     }
 }
